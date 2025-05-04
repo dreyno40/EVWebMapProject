4063692c4ef1910fd4ab08312b691b0b5787e66a
david <david@FIXED_DCR>
2025-05-04 10:24:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles(); // Serves index.html by default from wwwroot
+app.UseStaticFiles();  // Enables serving CSS, JS, images, etc.
+
+app.Run();
+
